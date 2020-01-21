003ac01a6fc5b3fdc16f6e80a3b57630ff6b6d6f
Matt Rudge <mrudge@gmail.com>
2020-01-21 13:25:21 +0000

    Remove pip upgrade because it breaks every time

--- init_tasks.sh
+++ init_tasks.sh
@@ -18,8 +18,8 @@ echo "Adding run aliases"
 echo 'alias run="python3 $GITPOD_REPO_ROOT/manage.py runserver 0.0.0.0:8000"' >> ~/.bashrc
 echo 'alias python=python3' >> ~/.bashrc
 echo 'alias pip=pip3' >> ~/.bashrc
-echo "Checking for pip upgrade"
-pip3 install --upgrade pip
+# echo "Checking for pip upgrade"
+# pip3 install --upgrade pip
 echo "Done"
 source ~/.bashrc
 rm $GITPOD_REPO_ROOT/.gitpod*
