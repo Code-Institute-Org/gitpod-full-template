3b3cd02966600bae7c51ad961e8c73b2a8ca28f9
Matt Rudge <mrudge@gmail.com>
2020-04-16 10:10:36 +0100

    feat: Add root user to mysql commands for Ubuntu 20.04 to init_tasks.sh
    Added root user to mysql commands for Ubuntu 20.04

--- .theia/init_tasks.sh
+++ .theia/init_tasks.sh
@@ -9,9 +9,9 @@
 echo "Setting the greeting"
 sed -i "s/USER_NAME/$GITPOD_GIT_USER_NAME/g" ${GITPOD_REPO_ROOT}/README.md
 echo "Creating the ${C9_USER} user in MySQL"
-mysql -e "CREATE USER '${C9_USER}'@'%' IDENTIFIED BY '';"
+mysql -e "CREATE USER '${C9_USER}'@'%' IDENTIFIED BY '';" -u root
 echo "Granting privileges"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${C9_USER}'@'%' WITH GRANT OPTION;"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${C9_USER}'@'%' WITH GRANT OPTION;" -u root
 echo "Creating .sqliterc file"
 echo ".headers on" > ~/.sqliterc
 echo ".mode column" >> ~/.sqliterc
