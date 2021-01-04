099f526a3fa14473b69cb993302e71f624df92af
Matt Rudge <matt.rudge@codeinstitute.net>
2021-01-04 08:37:21 +0000

    check if gitpod user already exists in mysql

--- .theia/init_tasks.sh
+++ .theia/init_tasks.sh
@@ -9,9 +9,14 @@
 echo "Setting the greeting"
 sed -i "s/USER_NAME/$GITPOD_GIT_USER_NAME/g" ${GITPOD_REPO_ROOT}/README.md
 echo "Creating the ${C9_USER} user in MySQL"
-mysql -e "CREATE USER '${C9_USER}'@'%' IDENTIFIED BY '';" -u root
-echo "Granting privileges"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${C9_USER}'@'%' WITH GRANT OPTION;" -u root
+RESULT="$(mysql -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '${C9_USER}')")"
+if [ "$RESULT" = 1 ]; then
+  echo "${C9_USER} already exists"
+else
+  mysql -e "CREATE USER '${C9_USER}'@'%' IDENTIFIED BY '';" -u root
+  echo "Granting privileges"
+  mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${C9_USER}'@'%' WITH GRANT OPTION;" -u root
+fi
 echo "Creating .sqliterc file"
 echo ".headers on" > ~/.sqliterc
 echo ".mode column" >> ~/.sqliterc
