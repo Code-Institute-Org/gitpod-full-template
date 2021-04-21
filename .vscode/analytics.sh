e9014e433faede562589b85bfb13ccc8d52abb13
Matt Rudge <matt.rudge@codeinstitute.net>
2021-04-21 16:40:56 +0100

    Anonymize data

--- .vscode/analytics.sh
+++ .vscode/analytics.sh
@@ -7,7 +7,7 @@ export UUID=$(cat /proc/sys/kernel/random/uuid)
 
 while true; do
 
-    DATA="-F date=$(date +%d/%m/%y) -F time=$(date +%H:%M:%S) -F user=${GITPOD_GIT_USER_EMAIL} -F repo=${GITPOD_WORKSPACE_CONTEXT_URL} -F uuid=${UUID}"
+    DATA="-F date=$(date +%d/%m/%y) -F time=$(date +%H:%M:%S) -F uuid=${UUID}"
 
     curl -X POST $DATA https://hooks.zapier.com/hooks/catch/4753778/ov8o90g/silent/
 
