736432af824023ae86e06a1794af50f8a6bdc1b8
Matt Rudge <matt.rudge@codeinstitute.net>
2021-04-21 16:22:31 +0100

    Update startup command to run in background

--- .vscode/analytics.sh
+++ .vscode/analytics.sh
@@ -3,6 +3,13 @@
 # Pings the webhook so that we can gather
 # basic usage stats
 
-DATA="-F date=$(date +%d/%m/%y) -F time=$(date +%H:%M:%S) -F user=${GITPOD_GIT_USER_EMAIL} -F repo=${GITPOD_WORKSPACE_CONTEXT_URL}"
+export UUID=$(cat /proc/sys/kernel/random/uuid)
 
-curl -X POST $DATA https://hooks.zapier.com/hooks/catch/4753778/ov8o90g
+while true; do
+
+    DATA="-F date=$(date +%d/%m/%y) -F time=$(date +%H:%M:%S) -F user=${GITPOD_GIT_USER_EMAIL} -F repo=${GITPOD_WORKSPACE_CONTEXT_URL} -F uuid=${UUID}"
+
+    curl -X POST $DATA https://hooks.zapier.com/hooks/catch/4753778/ov8o90g/silent/
+
+    sleep 300
+done
