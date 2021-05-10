46e70fe0f363720030a5c4caac2498b86456ea11
Matt Rudge <matt.rudge@codeinstitute.net>
2021-05-10 09:32:46 +0100

    Add alias for heroku_config

--- .vscode/init_tasks.sh
+++ .vscode/init_tasks.sh
@@ -22,6 +22,7 @@ echo ".headers on" > ~/.sqliterc
 echo ".mode column" >> ~/.sqliterc
 echo "Adding run aliases"
 echo 'alias run="python3 $GITPOD_REPO_ROOT/manage.py runserver 0.0.0.0:8000"' >> ~/.bashrc
+echo 'alias heroku_config=". $GITPOD_REPO_ROOT/.vscode/heroku_config.sh"' >> ~/.bashrc
 echo 'alias python=python3' >> ~/.bashrc
 echo 'alias pip=pip3' >> ~/.bashrc
 echo "Your workspace is ready to use. Happy coding!"
