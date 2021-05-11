1a47af41e828c7f88bd6c2ba482304fc799dc3d0
Matt Rudge <matt.rudge@codeinstitute.net>
2021-05-11 10:26:24 +0100

    make heroku_config more verbose

--- .vscode/heroku_config.sh
+++ .vscode/heroku_config.sh
@@ -7,10 +7,16 @@
 echo Heroku authentication configuration script
 echo Code Institute, 2021
 echo
+echo Get your Heroku API key by going to https://dashboard.heroku.com
+echo Go to Account Settings and click on Reveal to view your Heroku API key
+echo 
 
 if [[ -z "${HEROKU_API_KEY}" ]]; then
-   echo Paste your Heroku API key here:
+   echo Paste your Heroku API key here or press Enter to quit:
    read apikey
+   if [[ -z "${apikey}" ]]; then
+      return 0
+   fi
    echo export HEROKU_API_KEY=${apikey} >> ~/.bashrc
    echo Added the export. Refreshing the terminal.
    . ~/.bashrc > /dev/null
