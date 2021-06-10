b51c1b1c9be841230716dd8f33a0ac1443b84688
Matt Rudge <matt.rudge@codeinstitute.net>
2021-06-10 12:21:47 +0100

    Delete update_bashrc.sh

--- .vscode/update_bashrc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Adding run aliases"
-echo 'alias run="python3 $GITPOD_REPO_ROOT/manage.py runserver 0.0.0.0:8000"' >> ~/.bashrc
-echo 'alias heroku_config=". $GITPOD_REPO_ROOT/.vscode/heroku_config.sh"' >> ~/.bashrc
-echo 'alias python=python3' >> ~/.bashrc
-echo 'alias pip=pip3' >> ~/.bashrc
-echo 'python3 $GITPOD_REPO_ROOT/.vscode/font_fix.py' >> ~/.bashrc
-echo "Your workspace is ready to use. Happy coding!"
