4f0099a49b6e2dacb5ce06863d701d65145d4a52
Matt Rudge <matt.rudge@codeinstitute.net>
2021-07-27 09:34:33 +0100

    Make gitpod user explicit

--- .vscode/init_tasks.sh
+++ .vscode/init_tasks.sh
@@ -8,14 +8,14 @@
 
 echo "Setting the greeting"
 sed -i "s/USER_NAME/$GITPOD_GIT_USER_NAME/g" ${GITPOD_REPO_ROOT}/README.md
-echo "Creating the ${C9_USER} user in MySQL"
-RESULT="$(mysql -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '${C9_USER}')")"
+echo "Creating the gitpod user in MySQL"
+RESULT="$(mysql -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'gitpod')")"
 if [ "$RESULT" = 1 ]; then
-  echo "${C9_USER} already exists"
+  echo "gitpod already exists"
 else
-  mysql -e "CREATE USER '${C9_USER}'@'%' IDENTIFIED BY '';" -u root
+  mysql -e "CREATE USER 'gitpod'@'%' IDENTIFIED BY '';" -u root
   echo "Granting privileges"
-  mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${C9_USER}'@'%' WITH GRANT OPTION;" -u root
+  mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'gitpod'@'%' WITH GRANT OPTION;" -u root
 fi
 echo "Creating .sqliterc file"
 echo ".headers on" > ~/.sqliterc
