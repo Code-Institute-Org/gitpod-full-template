afe34ac84331933f799ac3829987645698d8f01f
Dave Horrocks <80708826+DaveyJH@users.noreply.github.com>
2022-05-18 00:16:02 +0100

    feat: Add `reset` to trigger API key removal

--- .vscode/heroku_config.sh
+++ .vscode/heroku_config.sh
@@ -22,5 +22,15 @@ if [[ -z "${HEROKU_API_KEY}" ]]; then
    . ~/.bashrc > /dev/null
    echo Done!
 else
-   echo API key is already set. Exiting
+   echo API key is already set.
+   echo
+   echo To reset the API key please input `reset`:
+   read reset_trigger
+   if [[ ${reset_trigger} == reset ]]; then
+      unset HEROKU_API_KEY
+      echo API key removed!
+   else
+      echo API key unchanged.
+   fi
+   echo Exiting
 fi
